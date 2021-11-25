16dafa2e9099056401bd877e607cb657839e96d9
Florian Fay <florian.fay@mni.thm.de>
2021-11-25 13:42:09 +0100

    Adds simple Shooting and Health

--- Game/Assets/Scripts/ShootGun.cs
+++ Game/Assets/Scripts/ShootGun.cs
@@ -5,22 +5,19 @@ using UnityEngine;
 
 public class ShootGun : NetworkBehaviour
 {
-
-    [SerializeField] int gunDamage = 1;
-    [SerializeField] float fireRate = 0.25f;
-    [SerializeField] float weaoponRange = 50f;
-    [SerializeField] Transform gunEnd;
-    [SerializeField] GameObject gun;
-
-    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
-    //private LineRenderer laserLine;
-    private float nextFire;
+    [SerializeField] int gunDamage = 10; // Damage output for gun, maybe attached to Gun-Object later
+    [SerializeField] float fireRate = 0.25f; // Firerate for gun, maybe attached to Gun-Object later
+    [SerializeField] float weaoponRange = 50f; // Range for gun, maybe attached to Gun-Object later
+    [SerializeField] Transform gunEnd; // Gun end for Animations, maybe attached to Gun-Object later 
+    [SerializeField] GameObject gun; // Gun-Object, can change
+    private float nextFire; // Time of the next shot you can take 
 
 
     public override void OnStartLocalPlayer()
     {
-        //laserLine = gun.GetComponent<LineRenderer>();
+        
     }
+
 
     void Update()
     {
@@ -32,27 +29,54 @@ public class ShootGun : NetworkBehaviour
         if (Input.GetButtonDown("Fire1") && Time.time > nextFire) 
         {
             nextFire = Time.time + fireRate;
-
             Shoot();
         }
         
     }
 
+    // Gets called if Player is hit, change later for Hit on Enemy
     [Command]
-    void CmdShoot(Vector3 hit)
+    void CmdShootPlayer(GameObject player)
     {
-       Debug.Log("Hit!");
+       Debug.Log("Hit Player!");
+       player.GetComponent<Health>().TakeDemage(gunDamage);
     }
 
+    // Function for hit on wall, can start Animation or something on point of hit
+    [ClientRpc]
+    void RpcHitWall(Vector3 hit)
+    {
+        Debug.Log("Hit Wall!");
+    }
+
+    // Gets called if Wall is hit
+    [Command]
+    void CmdShootWall(Vector3 hit)
+    {
+        RpcHitWall(hit);
+    }
+
+
+    /**
+    Function for shooting, casts Ray and calls functions based on object that is hit
+    */
     void Shoot()
     {
-        Vector3 rayOrigin = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
-
-        if(Physics.Raycast(rayOrigin, Camera.main.transform.forward, out hit, weaoponRange)) 
+        Vector3 rayOrigin = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        Vector3 direction = Camera.main.transform.forward;
+        
+        if(Physics.Raycast(rayOrigin, direction, out hit, weaoponRange)) 
         {
             Debug.Log("In Range!");
-            CmdShoot(hit.point);
+            if(hit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                CmdShootPlayer(hit.collider.gameObject.transform.parent.gameObject); // Gets Parent of Collider
+            }
+            else
+            {
+                CmdShootWall(hit.point);
+            }
         }
         else 
         {
