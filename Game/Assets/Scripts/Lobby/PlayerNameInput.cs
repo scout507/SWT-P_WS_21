f2d6aae929e343d3722d9537a61d2a73f8d51f95
socke300 <60921761+socke300@users.noreply.github.com>
2021-12-10 17:51:35 +0100

    Add documentation for PlayerNameInput

--- Game/Assets/Scripts/Lobby/PlayerNameInput.cs
+++ Game/Assets/Scripts/Lobby/PlayerNameInput.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/// <summary>This class is needed to validate the name input in the lobby UI.</summary>
 public class PlayerNameInput : MonoBehaviour
 {
     [Header("UI")]
+    /// <summary>Holds the input field for the name input.</summary>
     [SerializeField] private TMP_InputField nameInput = null;
+
+    /// <summary>Holds the next UI element, after the name input.</summary>
     [SerializeField] private GameObject hostMenu = null;
+
+    /// <summary>Contains the UI of the class itself.</summary>
     [SerializeField] private GameObject nameMenu = null;
 
-    public static string playerName {get; private set;} = "";
+    /// <summary>Static variable containing the player name.</summary>
+    public static string playerName = "";
 
+    /// <summary>
+    /// Reacts to the interaction of a button.
+    /// Checks if the input field of the name is empty.
+    /// If the input is not empty, the playername is set and the
+    /// UI elmente are changed by de- and activating them.
+    /// </summary>
     public void continueButtonPressed()
     {
-
         if (!string.IsNullOrEmpty(nameInput.text))
         {
             playerName = nameInput.text;
