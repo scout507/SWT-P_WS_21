8d8fd076be928c94c24e45939dfd1dd2a83f25e9
konstyy <konstantin.seen@gmail.com>
2021-12-13 13:45:49 +0100

    Compass Bar/ Task List Added
    Old concept of CompassBar removed, and a new Version is implemented
    
    The UI for a Task List is added too.

---
+++ Game/Assets/Scripts/Compass.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    public RawImage compassImage;
+    public Transform player;
+
+    private void Update() 
+    {
+        compassImage.uvRect = new Rect(player.localEulerAngles.y / 360f, 0f, 1f , 1f);
+        //Debug.Log(player.localEulerAngles.y);
+    }
+}
