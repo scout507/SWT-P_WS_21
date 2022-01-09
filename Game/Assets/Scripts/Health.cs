63d1a34f69df5b75fc47e01b0abaa658e9c9d99c
konstyy <64797633+konstyy@users.noreply.github.com>
2022-01-09 15:20:29 +0100

    GUI (#54)
    * Healthbar Object added
    
    The Player now has an Healthbar Object, but yet without function
    
    * added the healthbar
    
    the healthbar concept works, however It doesnt connect with an Object for some Reason.
    Still needs to be fixed.
    
    * Compass Bar/ Task List Added
    
    Old concept of CompassBar removed, and a new Version is implemented
    
    The UI for a Task List is added too.
    
    * Documentation Added, old Files removed
    
    the scripts are now explained and and old Compass Bar concept has been removed.
    
    * Clean Up
    
    unnecessary Debugs deleted, and HealthScript back to the "old" concept
    
    * Remove ui elements from player prefab
    
    * Format file
    
    * Delete TextmeshPro examples
    
    * Delete unnecessary meta file.
    
    * Change comments
    
    * Remove healthbar from health.cs
    
    * Change comment style
    
    Co-authored-by: socke300 <60921761+socke300@users.noreply.github.com>

--- Game/Assets/Scripts/Health.cs
+++ Game/Assets/Scripts/Health.cs
@@ -3,40 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-
-public class Health : NetworkBehaviour 
+public class Health : NetworkBehaviour
 {
+    /// <summary>
+    /// Variable for Health, synced on all Clients
+    /// </summary>
     [SyncVar]
-    public int health = 100; // Variable for Health, synced on all Clients
+    public int health = 100;
 
-    /**
-    Function for taking Damage, runs on Server
-    */
+    /// <summary>
+    /// Function for taking Damage, runs on Server
+    /// </summary>
+    /// <param name="amount"></param>
     public void TakeDamage(int amount)
     {
-        if(!isServer) return;
+        if (!isServer)
+            return;
 
         health -= amount;
+
         TargetDamage(amount);
-        if(health <= 0)
+        if (health <= 0)
         {
             TargetDeath();
         }
     }
 
-    /**
-    Function for taking Damage, runs on Client for Effects on Screen and so on
-    */
+    /// <summary>
+    /// Function for taking Damage, runs on Client for Effects on Screen and so on
+    /// </summary>
+    /// <param name="amount"></param>
     [TargetRpc]
     public void TargetDamage(int amount)
     {
         Debug.Log("Took damage:" + amount);
     }
 
-    /**
-    Death function for Client
-    Dummy at the moment
-    */
+    /// <summary>
+    /// Death function for Client
+    /// Dummy at the moment
+    /// </summary>
     [TargetRpc]
     void TargetDeath()
     {
