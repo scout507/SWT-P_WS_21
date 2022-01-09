2ba4b68d2aa4e4c4a02c859697064055013d7e37
yori21 <mail@alexochs.de>
2022-01-09 21:44:36 +0100

    Fix networking for new player prefab

--- Game/Assets/Scripts/PlayerAnimationStateController.cs
+++ Game/Assets/Scripts/PlayerAnimationStateController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class PlayerAnimationStateController : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class PlayerAnimationStateController : MonoBehaviour
     /// Player's Movement Script
     /// </summary>
     [SerializeField] PlayerMovement playerMovement;
+
+    /// <summary>
+    /// Player's Rig
+    /// </summary>
+    [SerializeField] Rig rig;
 
     // Update is called once per frame
     void Update()
@@ -31,10 +37,12 @@ public class PlayerAnimationStateController : MonoBehaviour
         if (taunt > 0)
         {
             animator.SetLayerWeight(2, 1);
+            rig.weight = 0;
         }
         else 
         {
             animator.SetLayerWeight(2, 0);
+            rig.weight = 1;
         }
     }
 }
