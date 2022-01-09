e2e0025bc5b2841e8709690105f751555e90abee
socke300 <60921761+socke300@users.noreply.github.com>
2022-01-09 12:31:53 +0100

    Revert "GUI (#44)"
    This reverts commit a0c11b72ea5fe8bdd74ff8e97e0359d6e5720b85.

--- Game/Assets/Scripts/Health.cs
+++ Game/Assets/Scripts/Health.cs
@@ -9,13 +9,6 @@ public class Health : NetworkBehaviour
     [SyncVar]
     public int health = 100; // Variable for Health, synced on all Clients
 
-    public HealthBar healthBar;
-
-    void Start()
-    {
-        healthBar.SetMaxHealth(health);
-    }
-
     /**
     Function for taking Damage, runs on Server
     */
@@ -24,7 +17,6 @@ public class Health : NetworkBehaviour
         if(!isServer) return;
 
         health -= amount;
-        healthBar.SetHealth(health);
         TargetDamage(amount);
         if(health <= 0)
         {
