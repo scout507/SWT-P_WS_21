f8e187ad67a13049a7b59ddf3d8ceaaa47ec983a
Dominik <70096457+scout507@users.noreply.github.com>
2022-01-10 15:11:25 +0100

    Adds Task Class (#60)
    * Advanced Player Movement (#43)
    
    * Add Sprinting
    
    Add the ability to sprint (with stamina + cooldown)
    
    * Add Crouching
    
    Add the ability to crouch
    
    * Sync player scale between clients
    
    Enabled synchronization of players scale between clients
    
    * Minor changes
    
    - Add prone stance, not using it due to bug
    
    - Enable scale interpolation via network
    
    - Sprinting cancels crouch
    
    * format file
    
    Co-authored-by: socke300 <60921761+socke300@users.noreply.github.com>
    
    * GUI (#44)
    
    * Healthbar Object added
    
    The Player now has an Healthbar Object, but yet without function
    
    * added the healthbar
    
    the healthbar concept works, however It doesnt connect with an Object for some Reason.
    Still needs to be fixed.
    
    * Compass Bar/ Task List Added
    
    Old concept of CompassBar removed, and a new Version is implemented
    
    The UI for a Task List is added too.
    
    * Documentation Added, old Files removed
    
    the scripts are now explained and and old Compass Bar concept has been removed.
    
    * Clean Up
    
    unnecessary Debugs deleted, and HealthScript back to the "old" concept
    
    * Copy file because of merge conflict.
    
    Co-authored-by: socke300 <60921761+socke300@users.noreply.github.com>
    
    * Adds Task-class
    
    * Adds additional comments
    
    * Adds spawn transform
    
    * Adds id, name and description to tasks
    
    * Fixes formating
    
    * Changes spawn from Transform to Vector3
    
    * Changes Trigger to only run on server
    
    * Changes players list to SyncVar
    
    * Revert "Changes players list to SyncVar"
    
    This reverts commit b8a191ef89ecbac22a78a7f7dce1cbc3a5f4aec2.
    
    * Changes player-list to SyncVar
    
    * Changes playerlist to Synclist
    
    * Changes protection level of some variables
    
    * Changes some variables to SyncVar
    
    * Changes player list to contain netIds
    
    * Adds readonly to synclist
    
    * FinishTask now sets progress to 100%
    
    * Removes TMP files
    
    * Adds additional comments
    
    * Fixes Whitespaces
    
    Co-authored-by: yori21 <alex.ochs@mni.thm.de>
    Co-authored-by: socke300 <60921761+socke300@users.noreply.github.com>
    Co-authored-by: konstyy <64797633+konstyy@users.noreply.github.com>

--- Game/Assets/Scripts/Task.cs
+++ Game/Assets/Scripts/Task.cs
@@ -5,50 +5,72 @@ using Mirror;
 
 public class Task : NetworkBehaviour
 {
+    /// <summary>True when the task is ready to be interacted with.</summary>
+    [SyncVar] public bool active;
+    /// <summary>True when the task is done.</summary>
+    [SyncVar] public bool done;
+    /// <summary>The place where the task should spawn.</summary>
+    public Vector3 spawn;
+    /// <summary>Id of the task, assinged by the TaskManager. Can be used to identify the Task.</summary>
+    public int id;
+    /// <summary>Name of the task for the UI.</summary>
+    public string taskName;
+    /// <summary>Short description for the UI.</summary>
+    public string taskDescription;
 
-    [SyncVar]Color color = Color.red;
+    /// <summary>Progress in percent. 50% should be 0.5</summary>
+    [SyncVar] public float progress;
+    /// <summary>List of all Players within the interactable radius</summary>
+    readonly public SyncList<uint> players = new SyncList<uint>();
 
+    DestructableObject dObjScript;
 
-    private bool touchedGround =false;
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Gets all dependencies
+    /// </summary>
+    private void Start()
     {
-        
-
-        
+        dObjScript = GetComponent<DestructableObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /// <summary>
+    /// Used to upgrade the health while building the task.
+    /// </summary>
+    private void UpdateHealth()
     {
-        gameObject.GetComponent<Renderer>().material.color = color;
-       
+        dObjScript.SetHealth(progress);
+    }
 
-        
+    /// <summary>
+    /// Used to finish the task.
+    /// </summary>
+    private void FinishTask()
+    {
+        active = false;
+        done = true;
+        progress = 1;
+        UpdateHealth();
+    }
+
+    /// <summary>
+    /// Adds players to the players List once they are in the interactive radius.
+    /// </summary>
+    /// <param name="other">Collider of the entering GameObject</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && isServer) players.Add(other.GetComponent<NetworkIdentity>().netId);
     }
 
 
- 
- 
-    void OnCollisionEnter (Collision other){
-        
-
-        changeColor();
-        
-   
-        
-        touchedGround = true;
-        
-        
+    /// <summary>
+    /// Removes players from the player list when they leave the interactive radius.
+    /// </summary>
+    /// <param name="other">Collider of the exiting GameObject</param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" && isServer) players.Remove(other.GetComponent<NetworkIdentity>().netId);
     }
- 
-    void changeColor(){
-        if (touchedGround){
-            Debug.Log("Collided");
-           color = Color.green;
-            
 
-        }
 
-    }
 }
