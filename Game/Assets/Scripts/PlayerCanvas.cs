aea9ab99b00a1058b538ff796d470991eaa1ae58
Konstantin Seen <konstantin.seen@mni.thm.de>
2022-03-01 21:30:47 +0100

    Update PlayerCanvas.cs

--- Game/Assets/Scripts/PlayerCanvas.cs
+++ Game/Assets/Scripts/PlayerCanvas.cs
@@ -8,15 +8,21 @@ public class PlayerCanvas : NetworkBehaviour
 {
     public GameObject PlayerCanvasObject;
     private Inventory inventory;
+    // Start is called before the first frame update
     void Start()
     {
-        if(isLocalPlayer){
+        if(isLocalPlayer)
+        {
             PlayerCanvasObject.SetActive(true);
         }
     }
 
+    /// <summary>
+    /// Updates the current inventory text and icon to the currently wielded weapon
+    /// </summary>
+    /// <param name="newWeapon"></param>
     public void UpdateWeaponUI(ShootGun newWeapon)
     {
-        inventory.UpdateInfo(newWeapon.icon,newWeapon.gunAmmo, 0);
+        inventory.UpdateInfo(newWeapon.icon, newWeapon.gunAmmo, 0);
     }
 }
