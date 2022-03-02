bd419b795ead7f8d2ac6be15f191830ecc14a256
socke300 <60921761+socke300@users.noreply.github.com>
2022-03-02 13:56:55 +0100

    Add documentation

--- Game/Assets/Scripts/Menu/PlayerSpawnSystem.cs
+++ Game/Assets/Scripts/Menu/PlayerSpawnSystem.cs
@@ -9,13 +9,22 @@ using System.Linq;
 /// </summary>
 public class PlayerSpawnSystem : NetworkBehaviour
 {
-
     /// <summary>
-    /// Holds the prefab for the player that will be spawned.
+    /// Holds the prefabs for the player that can be spawned.
+    /// Requires a minimum and maximum of 8 prefabs.
     /// </summary>
-    [SerializeField] private GameObject playerPrefab = null;
+    [SerializeField]
+    private List<GameObject> playerPrefabs = null;
 
     /// <summary>
+    /// Contains the default prefab for the player that can be spawned 
+    /// if there is no prefab left in the "playerPrefabs" list.
+    /// </summary>
+    [SerializeField]
+    private GameObject playerPrefabDefault = null;
+
+    /// <summary>
+    /// Called when the class will been instantiated.
     /// Add the Function "SpawnPlayer()" to the Networkmanager.
     /// </summary>
     public override void OnStartServer()
@@ -24,17 +33,33 @@ public class PlayerSpawnSystem : NetworkBehaviour
         base.OnStartServer();
     }
 
+    /// <summary>
+    /// Called when the class will been Destroyed.
+    /// Removes the Function "SpawnPlayer()" to the Networkmanager.
+    /// </summary>
     [ServerCallback]
     private void OnDestroy() => NetworkManagerLobby.OnServerReadied -= SpawnPlayer;
 
+    /// <summary>
+    /// Called only by the server.
+    /// Randomly creates the player with one of the predefined presets/classes.
+    /// If no more prefab is available, the default prefab is used.
+    /// </summary>
+    /// <param name="conn">Network connection from the client.</param>
     [Server]
     public void SpawnPlayer(NetworkConnection conn)
     {
+        GameObject playerInstance;
 
-        GameObject playerInstance = Instantiate(playerPrefab);
-        //NetworkServer.Spawn(playerInstance, conn);
+        if (playerPrefabs.Count == 0)
+            playerInstance = Instantiate(playerPrefabDefault);
+        else
+        {
+            int index = new System.Random().Next(playerPrefabs.Count);
+            playerInstance = Instantiate(playerPrefabs[index]);
+            playerPrefabs.RemoveAt(index);
+        }
+
         NetworkServer.ReplacePlayerForConnection(conn, playerInstance.gameObject, true);
     }
-
-
 }
