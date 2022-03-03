180d89a8fe7090ce71165cf1d8aafc7184299fe8
socke300 <socke300@users.noreply.github.com>
2022-03-03 09:45:02 +0000

    deploy: dd6475176d09abeebdd87f62db8819640838f517

--- search/classes_f.js
+++ search/classes_f.js
@@ -8,11 +8,12 @@ var searchData=
   ['playernameinput_5',['PlayerNameInput',['../class_player_name_input.html',1,'']]],
   ['playerspawnpoint_6',['PlayerSpawnPoint',['../class_player_spawn_point.html',1,'']]],
   ['playerspawnsystem_7',['PlayerSpawnSystem',['../class_player_spawn_system.html',1,'']]],
-  ['pool_8',['Pool',['../classkcp2k_1_1_pool.html',1,'kcp2k.Pool&lt; T &gt;'],['../class_mirror_1_1_pool.html',1,'Mirror.Pool&lt; T &gt;'],['../class_telepathy_1_1_pool.html',1,'Telepathy.Pool&lt; T &gt;']]],
-  ['pool_3c_20byte_5b_5d_3e_9',['Pool&lt; byte[]&gt;',['../class_telepathy_1_1_pool.html',1,'Telepathy']]],
-  ['pool_3c_20mirror_3a_3apoolednetworkreader_20_3e_10',['Pool&lt; Mirror::PooledNetworkReader &gt;',['../class_mirror_1_1_pool.html',1,'Mirror']]],
-  ['pool_3c_20mirror_3a_3apoolednetworkwriter_20_3e_11',['Pool&lt; Mirror::PooledNetworkWriter &gt;',['../class_mirror_1_1_pool.html',1,'Mirror']]],
-  ['pool_3c_20segment_20_3e_12',['Pool&lt; Segment &gt;',['../classkcp2k_1_1_pool.html',1,'kcp2k']]],
-  ['poolednetworkreader_13',['PooledNetworkReader',['../class_mirror_1_1_pooled_network_reader.html',1,'Mirror']]],
-  ['poolednetworkwriter_14',['PooledNetworkWriter',['../class_mirror_1_1_pooled_network_writer.html',1,'Mirror']]]
+  ['playertasksui_8',['PlayerTasksUI',['../class_player_tasks_u_i.html',1,'']]],
+  ['pool_9',['Pool',['../classkcp2k_1_1_pool.html',1,'kcp2k.Pool&lt; T &gt;'],['../class_mirror_1_1_pool.html',1,'Mirror.Pool&lt; T &gt;'],['../class_telepathy_1_1_pool.html',1,'Telepathy.Pool&lt; T &gt;']]],
+  ['pool_3c_20byte_5b_5d_3e_10',['Pool&lt; byte[]&gt;',['../class_telepathy_1_1_pool.html',1,'Telepathy']]],
+  ['pool_3c_20mirror_3a_3apoolednetworkreader_20_3e_11',['Pool&lt; Mirror::PooledNetworkReader &gt;',['../class_mirror_1_1_pool.html',1,'Mirror']]],
+  ['pool_3c_20mirror_3a_3apoolednetworkwriter_20_3e_12',['Pool&lt; Mirror::PooledNetworkWriter &gt;',['../class_mirror_1_1_pool.html',1,'Mirror']]],
+  ['pool_3c_20segment_20_3e_13',['Pool&lt; Segment &gt;',['../classkcp2k_1_1_pool.html',1,'kcp2k']]],
+  ['poolednetworkreader_14',['PooledNetworkReader',['../class_mirror_1_1_pooled_network_reader.html',1,'Mirror']]],
+  ['poolednetworkwriter_15',['PooledNetworkWriter',['../class_mirror_1_1_pooled_network_writer.html',1,'Mirror']]]
 ];
--- search/variables_f.js
+++ search/variables_f.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['taskdescription_0',['taskDescription',['../class_task.html#abc1112df9f1cc4353af76bea1da74d9f',1,'Task']]],
   ['taskname_1',['taskName',['../class_task.html#a15816482f88ca69dfbe9a7498ecf262b',1,'Task']]],
-  ['timeperplankinseconds_2',['timePerPlankInSeconds',['../class_fence_interaction.html#a85ab84ca1105b8558d917a2bdf059938',1,'FenceInteraction']]],
-  ['toggle_3',['toggle',['../class_menu.html#a57d3268552b008bc4e74809824fbe475',1,'Menu']]],
-  ['totalplayers_4',['totalPlayers',['../class_round_manager.html#a11d22c2c18ed59a69bee5663e79c5037',1,'RoundManager']]]
+  ['taskscreated_2',['tasksCreated',['../class_task_manager.html#a1f6376ca8025c86afad8374a296f147f',1,'TaskManager']]],
+  ['timeperplankinseconds_3',['timePerPlankInSeconds',['../class_fence_interaction.html#a85ab84ca1105b8558d917a2bdf059938',1,'FenceInteraction']]],
+  ['toggle_4',['toggle',['../class_menu.html#a57d3268552b008bc4e74809824fbe475',1,'Menu']]],
+  ['totalplayers_5',['totalPlayers',['../class_round_manager.html#a11d22c2c18ed59a69bee5663e79c5037',1,'RoundManager']]]
 ];
