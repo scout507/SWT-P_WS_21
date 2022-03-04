ea66e23f9448386e4287ab8d3fb1e41eb302a27a
alexochs <alexochs@users.noreply.github.com>
2022-03-04 10:46:10 +0000

    deploy: 8bd378b3bc726d14edd8c0ebeebd0ce6dcb04a31

--- search/classes_0.js
+++ search/classes_0.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['addplayermessage_0',['AddPlayerMessage',['../struct_mirror_1_1_add_player_message.html',1,'Mirror']]],
   ['arraybuffer_1',['ArrayBuffer',['../class_mirror_1_1_simple_web_1_1_array_buffer.html',1,'Mirror::SimpleWeb']]],
-  ['authrequestmessage_2',['AuthRequestMessage',['../struct_mirror_1_1_authenticators_1_1_basic_authenticator_1_1_auth_request_message.html',1,'Mirror::Authenticators::BasicAuthenticator']]],
-  ['authresponsemessage_3',['AuthResponseMessage',['../struct_mirror_1_1_authenticators_1_1_basic_authenticator_1_1_auth_response_message.html',1,'Mirror::Authenticators::BasicAuthenticator']]]
+  ['audiocontroller_2',['AudioController',['../class_audio_controller.html',1,'']]],
+  ['authrequestmessage_3',['AuthRequestMessage',['../struct_mirror_1_1_authenticators_1_1_basic_authenticator_1_1_auth_request_message.html',1,'Mirror::Authenticators::BasicAuthenticator']]],
+  ['authresponsemessage_4',['AuthResponseMessage',['../struct_mirror_1_1_authenticators_1_1_basic_authenticator_1_1_auth_response_message.html',1,'Mirror::Authenticators::BasicAuthenticator']]]
 ];
--- search/functions_e.js
+++ search/functions_e.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['play_0',['Play',['../class_soundmanager.html#a4a3b9721303c77ca378ac5788dbb3312',1,'Soundmanager']]],
-  ['processclientrequest_1',['ProcessClientRequest',['../class_mirror_1_1_discovery_1_1_network_discovery_base.html#afd12e333f578fb4d3c0b615b980ca8dd',1,'Mirror::Discovery::NetworkDiscoveryBase']]],
-  ['processrequest_2',['ProcessRequest',['../class_mirror_1_1_discovery_1_1_network_discovery.html#a102b884161ed3cfbc83bbddd31aab504',1,'Mirror.Discovery.NetworkDiscovery.ProcessRequest()'],['../class_mirror_1_1_discovery_1_1_network_discovery_base.html#a08b153629b2e2074f10472875be470d0',1,'Mirror.Discovery.NetworkDiscoveryBase.ProcessRequest()']]],
-  ['processresponse_3',['ProcessResponse',['../class_mirror_1_1_discovery_1_1_network_discovery.html#a015a04e33084dd1d065f897ff2322ecd',1,'Mirror.Discovery.NetworkDiscovery.ProcessResponse()'],['../class_mirror_1_1_discovery_1_1_network_discovery_base.html#a681d1d1d8095ace1946b3a99bba14b44',1,'Mirror.Discovery.NetworkDiscoveryBase.ProcessResponse()']]]
+  ['playgunsound_1',['PlayGunSound',['../class_audio_controller.html#a25a9d490bca57d49ce4fac8abd1beb4a',1,'AudioController']]],
+  ['processclientrequest_2',['ProcessClientRequest',['../class_mirror_1_1_discovery_1_1_network_discovery_base.html#afd12e333f578fb4d3c0b615b980ca8dd',1,'Mirror::Discovery::NetworkDiscoveryBase']]],
+  ['processrequest_3',['ProcessRequest',['../class_mirror_1_1_discovery_1_1_network_discovery.html#a102b884161ed3cfbc83bbddd31aab504',1,'Mirror.Discovery.NetworkDiscovery.ProcessRequest()'],['../class_mirror_1_1_discovery_1_1_network_discovery_base.html#a08b153629b2e2074f10472875be470d0',1,'Mirror.Discovery.NetworkDiscoveryBase.ProcessRequest()']]],
+  ['processresponse_4',['ProcessResponse',['../class_mirror_1_1_discovery_1_1_network_discovery.html#a015a04e33084dd1d065f897ff2322ecd',1,'Mirror.Discovery.NetworkDiscovery.ProcessResponse()'],['../class_mirror_1_1_discovery_1_1_network_discovery_base.html#a681d1d1d8095ace1946b3a99bba14b44',1,'Mirror.Discovery.NetworkDiscoveryBase.ProcessResponse()']]]
 ];
