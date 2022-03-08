ef49b455f02c00a79294d6d54ec9ffa3c35b0c70
socke300 <socke300@users.noreply.github.com>
2022-03-08 17:08:15 +0000

    deploy: a7bbdd3f91ad71bec4ce539a53d148ba35302987

--- search/variables_a.js
+++ search/variables_a.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['matchid_0',['matchId',['../class_mirror_1_1_network_match.html#aadc97198bb715e3ad71ce47f7476c6f1',1,'Mirror::NetworkMatch']]],
-  ['maxconnections_1',['maxConnections',['../class_mirror_1_1_network_manager.html#a647cb7a86c0cd91fea090a047b914793',1,'Mirror::NetworkManager']]],
-  ['maxhealth_2',['maxHealth',['../class_destructable_object.html#a31395416684f05945fd1569586116da6',1,'DestructableObject']]],
-  ['menucanvas_3',['menuCanvas',['../class_ingame_menu.html#aabc7a7f88d46fc0478a9ba40bdf458a3',1,'IngameMenu']]],
-  ['message_4',['message',['../struct_mirror_1_1_network_diagnostics_1_1_message_info.html#adc49afbdaac53d89f9862817ae05d750',1,'Mirror::NetworkDiagnostics::MessageInfo']]],
-  ['movementspeed_5',['movementSpeed',['../class_spectator.html#ab1d81b467258f0cf21a790f80e5014f4',1,'Spectator']]],
-  ['movespeed_6',['moveSpeed',['../class_monster_controller.html#ac61a31efa277d192bc0fbaf8143bf783',1,'MonsterController']]]
+  ['magsize_0',['magSize',['../class_shoot_gun.html#a7c0f8ed476a9d68e3e97b43b8bfb0f01',1,'ShootGun']]],
+  ['matchid_1',['matchId',['../class_mirror_1_1_network_match.html#aadc97198bb715e3ad71ce47f7476c6f1',1,'Mirror::NetworkMatch']]],
+  ['maxconnections_2',['maxConnections',['../class_mirror_1_1_network_manager.html#a647cb7a86c0cd91fea090a047b914793',1,'Mirror::NetworkManager']]],
+  ['maxhealth_3',['maxHealth',['../class_destructable_object.html#a31395416684f05945fd1569586116da6',1,'DestructableObject']]],
+  ['menucanvas_4',['menuCanvas',['../class_ingame_menu.html#aabc7a7f88d46fc0478a9ba40bdf458a3',1,'IngameMenu']]],
+  ['message_5',['message',['../struct_mirror_1_1_network_diagnostics_1_1_message_info.html#adc49afbdaac53d89f9862817ae05d750',1,'Mirror::NetworkDiagnostics::MessageInfo']]],
+  ['movementspeed_6',['movementSpeed',['../class_spectator.html#ab1d81b467258f0cf21a790f80e5014f4',1,'Spectator']]],
+  ['movespeed_7',['moveSpeed',['../class_monster_controller.html#ac61a31efa277d192bc0fbaf8143bf783',1,'MonsterController']]]
 ];
