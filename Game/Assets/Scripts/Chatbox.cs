ecfaf947b512bf528e86e771beec49bcad63df8d
Dominik <dominik.muench@mni.thm.de>
2022-03-10 12:42:35 +0100

    Adds chatting

--- Game/Assets/Scripts/Chatbox.cs
+++ Game/Assets/Scripts/Chatbox.cs
@@ -8,14 +8,22 @@ public class Chatbox : NetworkBehaviour
 {
     /// <summary>The UI component holding the textbox.</summary>
     [SerializeField] GameObject textBoxUI;
+    //  <summary>The UI component holding the input field.</summary>
+    [SerializeField] TMP_InputField chatBoxUI;
     /// <summary>The text component of the textbox.</summary>
     TextMeshProUGUI textBox;
+    /// <summary>The text component of the input field.</summary>
+    TextMeshProUGUI chatText;
     /// <summary>Array holding the newest 5 messages</summary>
     string[] messages = new string[5];
     /// <summary>Time until a message is deleted</summary>
     float timeForDeletion = 25f;
     /// <summary>Timer for deletion</summary>
     float[] timer = new float[5];
+
+    string userInput = "";
+    string playerName;
+    bool typing;
 
     /// <summary>
     /// Fetches dependencies
@@ -25,6 +33,9 @@ public class Chatbox : NetworkBehaviour
         if (!isLocalPlayer) return;
 
         textBox = textBoxUI.GetComponent<TextMeshProUGUI>();
+        chatText = chatBoxUI.GetComponentInChildren<TextMeshProUGUI>();
+        playerName = GetComponent<Player>().displayName;
+
     }
 
     void Update()
@@ -32,6 +43,23 @@ public class Chatbox : NetworkBehaviour
         if (!isLocalPlayer) return;
         UpdateTimers();
         UpdateText();
+
+
+        if(typing)
+        {
+            if(Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                typing = false;
+                if(chatBoxUI.text.Length >0) SubmitMessage(chatText.text);
+            }
+            
+        }
+        if(Input.GetKeyDown(KeyCode.KeypadEnter) && !typing)
+        {
+            typing = true;
+            chatBoxUI.Select();
+        }
+        
     }
 
     /// <summary>
@@ -80,6 +108,50 @@ public class Chatbox : NetworkBehaviour
         }
 
         textBox.text = text;
+    }
+
+    void SubmitMessage(string message)
+    {
+        List<GameObject> nearbyPlayers = FindNearbyPlayers();
+
+        if(nearbyPlayers.Count > 0)
+        {
+            foreach(GameObject player in nearbyPlayers)
+            {
+                CmdSendMessage(playerName + ": " + message, player);
+            }
+        }
+
+        AddMessage("You: " + message);
+        chatBoxUI.text = "";
+    }
+
+
+    List<GameObject> FindNearbyPlayers()
+    {
+        List<GameObject> nearbyPlayers = new List<GameObject>();
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 40f);
+        
+        foreach (Collider hitCollider in hitColliders)
+        {
+            if(hitCollider.gameObject.tag == "Player" && !nearbyPlayers.Contains(hitCollider.gameObject) && hitCollider.gameObject != this.gameObject)
+            {
+                nearbyPlayers.Add(hitCollider.gameObject);
+            } 
+        }
+        return nearbyPlayers;
+    }
+
+    [Command]
+    void CmdSendMessage(string message, GameObject target)
+    {
+        TargetRPCSendMessage(target.GetComponent<NetworkIdentity>().connectionToClient, message);
+    }
+
+    [TargetRpc]
+    void TargetRPCSendMessage(NetworkConnection target, string message)
+    {
+        NetworkClient.localPlayer.gameObject.GetComponent<Chatbox>().AddMessage(message);
     }
 
 }
